e8500026d05aefcbd10718b0b71f396d9445ba9d
michailb <michailb@kmslh.com>
2025-05-29 04:12:16 +0300

    add-bookings
    - Fix getting last and next booking for item

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -5,7 +5,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import ru.practicum.shareit.booking.mapper.BookingMapper;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.CommentCreateRequest;
 import ru.practicum.shareit.item.dto.CommentDto;
